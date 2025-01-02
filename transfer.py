b503210360d026f40ffef28e21280b68bb86bbb9
Mustang-ing <aissapansan@hotmail.com>
2025-01-02 14:30:11 +0100

    Debut RDFLib

--- transfer.py
+++ transfer.py
@@ -1,4 +1,5 @@
 import csv
+from rdflib import URIRef,Literal,Namespace,Graph
 
 
 # Etape 1 : Importer le fichier via csv.reader
@@ -16,13 +17,22 @@ with open('ml-100k/u1.base', 'r') as csvfile:
     rating = [ int(x) for x in rating]
     #print(user_id)
     #print(item_id)
-    print(max(item_id))
+    #print(max(item_id))
 
     # Pour eviter de surcharger Protege, on va prendre un échantillon de 200 éléments, on ignore timestamp.
     
     user_id_sample = user_id[:200]
     item_id_sample = item_id[:200]
     rating_sample = rating[:200]
+
+    g = Graph() # Generation d'un objet RDF Graphe (Ontologie)
+    g.parse("Movie.rdf")
+    # print(g.serialize()) POur voir si l'ontologie est bien charger
+    #Objectif : Générer un nouvel inidividu prenant pour informations la 2eme ligne du data csv 
+
+    #Commencer par faire le namespace
+
+
     """
     for i in range(0,20):
         print(N1[i])
