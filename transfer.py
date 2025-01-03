1c29d704d90101d0fbf561bd6d69ed482f11e07d
Mustang-ing <aissapansan@hotmail.com>
2025-01-03 09:11:44 +0100

    Ajout de User2 via le script Python

--- transfer.py
+++ transfer.py
@@ -1,6 +1,6 @@
 import csv
 from rdflib import URIRef,Literal,Namespace,Graph
-from rdflib.namespace import RDF
+from rdflib.namespace import RDF,OWL
 
 # Etape 1 : Importer le fichier via csv.reader
 
@@ -20,34 +20,35 @@ with open('ml-100k/u1.base', 'r') as csvfile:
     #print(max(item_id))
 
     # Pour eviter de surcharger Protege, on va prendre un échantillon de 200 éléments, on ignore timestamp.
-    
+
     user_id_sample = user_id[:200]
     item_id_sample = item_id[:200]
     rating_sample = rating[:200]
 
-    g = Graph() # Generation d'un objet RDF Graphe (Ontologie)
+
+
+    # Define the namespace
+    Movie_namespace = Namespace("http://www.semanticweb.org/ing-mustang/ontologies/2024/11/Movie.owl#")
+
+    # Create the graph
+    g = Graph()
     g.parse("Movie.rdf")
-    #print(g.serialize()) #Pour voir si l'ontologie est bien charger
-    #Objectif : Générer un nouvel inidividu prenant pour informations la 2eme ligne du data csv 
 
-    #Commencer par faire le namespace
-    
-    Movie_namespace = Namespace("http://www.semanticweb.org/ing-mustang/ontologies/2024/11/Movie.owl")
-    
-    User2 = Movie_namespace.User2 # http://www.semanticweb.org/ing-mustang/ontologies/2024/11/Movie.owl/User2
-    user_id = Literal(user_id_sample[1])
-    movieId = Literal(item_id_sample[1])
-    rating = Literal(rating_sample[1])
+    #Bind the namespace to ensure proper prefixes
+    g.bind("Movie", Movie_namespace)
 
-    print(f"User2 : URI : {User2} | User_id : {user_id} | MovieId : {movieId} | Rating : {rating}")
-    
-    g.add((User2,RDF.type,Movie_namespace.Rating)) #Ajouter le triplet sur le type
-    g.add((User2,Movie_namespace.userId,user_id))  
-    g.add((User2,Movie_namespace.movieid,movieId))
-    g.add((User2,Movie_namespace.rate,rating))
+    # Create a new individual and add properties
+    User2 = Movie_namespace.User2
+    user_id = Literal(user_id_sample[1], datatype="http://www.w3.org/2001/XMLSchema#decimal")
+    movieId = Literal(item_id_sample[1], datatype="http://www.w3.org/2001/XMLSchema#decimal")
+    rating = Literal(rating_sample[1], datatype="http://www.w3.org/2001/XMLSchema#decimal")
 
-    #print(g.serialize())
-    
+    g.add((User2, RDF.type, OWL.NamedIndividual))  # Declare as NamedIndividual
+    g.add((User2, RDF.type, Movie_namespace.Rating))
+    g.add((User2, Movie_namespace.userId, user_id))
+    g.add((User2, Movie_namespace.movieId, movieId))
+    g.add((User2, Movie_namespace.rate, rating))
+
     RDF_file = g.serialize(format='xml')
     #print(RDF_file)
 
